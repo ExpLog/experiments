6806878210ed4e0fd6842a3299dc56569662dae7
Leonardo Fontoura <lobo.fontoura@gmail.com>
2019-07-23 15:14:31 +0100

    Adds maven-compiler-plugin so kotlin won't complain about java generated codee.
    Adds java source/target version.
    Adds a little bit of experimentation with jooq.
    Changes kotlin-stdlib to the jdk8 version.

--- src/main/kotlin/org/log2/jooq.kt
+++ src/main/kotlin/org/log2/jooq.kt
@@ -1,8 +1,10 @@
 package org.log2
 
+import org.jooq.Result
 import org.jooq.SQLDialect
 import org.jooq.impl.DSL
 import org.log2.jooq.model.Tables.ACTOR
+import org.log2.jooq.model.tables.records.ActorRecord
 import java.sql.DriverManager
 
 fun main(args: Array<String>) {
@@ -14,11 +16,16 @@ fun main(args: Array<String>) {
         val connection = DriverManager.getConnection(url, username, password)
 
         val create = DSL.using(connection, SQLDialect.POSTGRES_10)
-        val result = create.select().from(ACTOR).fetch()
+        val result: Result<ActorRecord> = create.selectFrom(ACTOR).fetch()
+        println(result[0].javaClass)
         println("Result: $result")
+
+        val result2 =
+            create.select(ACTOR.FIRST_NAME.concat(" ").concat(ACTOR.LAST_NAME).`as`("name")).from(ACTOR).fetch()
+        println(result2[0].javaClass)
+        println("Result: $result2")
 
     } catch (e: Exception) {
         e.printStackTrace()
     }
 }
-
